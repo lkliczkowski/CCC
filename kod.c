33a29758521486d858634f7f9e1c6931069de2e8
Łukasz Kliczkowski <lkliczkowski@p109-3.labpk.inf.ug.edu.pl>
2012-11-27 09:55:29 +0100

    Napisałem dobry kod!

---
+++ kod.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main ()
+{
+
+int liczba=0;
+double plik=2.3;
+float dzielnik=20;
+char literka=a;
+int a;
+int b;
+int c;
+int d;
+
+for(i=0;i<1-:i++){
+printf("Coś");
+scanf
+}
+return 0;
+}
